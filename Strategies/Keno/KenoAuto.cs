507fc0d371333aecd14931ee53ab07a86b85f64c
Mike Donovan <caffeine239@yahoo.com>
2024-05-10 14:55:34 -0400

    updatekeno

---
+++ Strategies/Keno/KenoAuto.cs
@@ -0,0 +1,34 @@
+﻿using StakeBuddy.Bots.CommonBot;
+using System.Collections.Generic;
+
+public class KenoAuto : Strategy
+{
+    List<int> selectedNumbers = new List<int>();
+
+    public override StakeOriginalGame DesignedForStakeOriginal
+    {
+        get
+        {
+            return StakeOriginalGame.Keno;
+        }
+    }
+
+    public override string Name
+    {
+        get
+        {
+            return "Default Keno Auto Play";
+        }
+    }
+    public override void Init()
+    {
+        SetBetAmount(GetMainBotBetAmount());
+        selectedNumbers = GetRandomKenoNumbers();
+        SetKenoNumbers(selectedNumbers);
+    }
+    public override void Tick()
+    {
+        bool wasWin;
+        List<int> result = (List<int>)DoBet(out wasWin);
+    }
+}
