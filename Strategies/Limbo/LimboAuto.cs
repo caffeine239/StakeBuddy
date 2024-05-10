78d6fac5a8ed9be61a14f23f4095d52ab797c009
Mike Donovan <caffeine239@yahoo.com>
2024-05-10 14:30:25 -0400

    Create LimboAuto.cs

---
+++ Strategies/Limbo/LimboAuto.cs
@@ -0,0 +1,33 @@
+﻿using StakeBuddy.Bots.CommonBot;
+
+public class LimboAuto : Strategy
+{
+    public override StakeOriginalGame DesignedForStakeOriginal
+    {
+        get
+        {
+            return StakeOriginalGame.Limbo;
+        }
+    }
+
+    public override string Name
+    {
+        get
+        {
+            return "Default Limbo Auto Play";
+        }
+    }
+    public override void Init()
+    {
+        SetBetAmount(GetMainBotBetAmount());
+        SetMultiplierTarget(GetMainMultiplierTarget());
+    }
+    public override void Tick()
+    {
+        SetBetAmount(GetMainBotBetAmount());
+        SetMultiplierTarget(GetMainMultiplierTarget());
+
+        bool wasWin;
+        double result = (double)DoBet(out wasWin);
+    }
+}
